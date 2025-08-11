48ad5810c8bb89c357b5b7aae1233e67ef327e35
Dody <welcom@gmail.com>
2025-08-12 02:52:59 +0300

    some edit here

---
+++ Data/Modle/Dto/UserTasksResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using To_Do.Data.Modle.Dto.TaskDto;
+
+namespace To_Do.Data.Modle.Dto
+{
+    public class UserTasksResponseDto
+    {
+        public UserResponceDto? User { get; set; } 
+
+       public  List<TaskResponceDto>? Tasks { get; set; }  
+
+
+    }
+}
